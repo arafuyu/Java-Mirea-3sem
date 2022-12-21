0ab9fb5389f805eb64fde5e831f9b2461ff818b5
Nikita <112932000+arafuyu@users.noreply.github.com>
2022-12-21 18:18:53 +0300

    Add files via upload

---
+++ Pract 1 (old)/Dog/TestDog.java
@@ -0,0 +1,25 @@
+package Dog;
+
+public class TestDog {
+    public static void main (String[] args) {
+        Dog d1 = new Dog("Mike", 2);
+        Dog d2 = new Dog("Helen", 7);
+        Dog d3 = new Dog("Bob");
+        d3.setAge(1);
+        System.out.println();
+        System.out.println(d2+"\n");
+        d1.intoHumanAge();
+        d2.intoHumanAge();
+        d3.intoHumanAge();
+    }
+}
+
+
+
+
+
+
+
+
+
+
