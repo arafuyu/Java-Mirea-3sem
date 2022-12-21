1b5c270e07badcc4b52c3765b5a21efcda50a0ed
Nikita <112932000+arafuyu@users.noreply.github.com>
2022-12-21 19:03:36 +0300

    Add files via upload

---
+++ Pract 7 (old)/Main.java
@@ -0,0 +1,25 @@
+package Pract7;
+
+public class Main {
+    public static void main(String[] args) {
+        run DrunkGame = new run();
+
+        DrunkGame.setDecks();
+        DrunkGame.getDecks();
+        DrunkGame.setStacks();
+
+        DrunkGame.CompareStacks();
+    }
+}
+
+//08642
+//97531
+//botva
+
+//85296
+//04721
+//2 33
+
+//24680
+//13579
+//1 5
