e52962c422c850aff9c28e41df1f0607946053eb
Nikita <112932000+arafuyu@users.noreply.github.com>
2022-12-21 18:23:07 +0300

    Add files via upload

---
+++ Pract 2 (old)/Author/TestAuthor.java
@@ -0,0 +1,8 @@
+package Author;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author author = new Author("Наташа", "natasha2709@mirea.ru", 'U');
+        System.out.println(author);
+    }
+}
